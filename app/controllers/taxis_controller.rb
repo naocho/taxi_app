0348f6d15a47846b7815a3135016183dd80898eb
naocho <kirateiru831@gmail.com>
2020-10-03 11:45:30 +0900

    投稿機能完了

--- app/controllers/taxis_controller.rb
+++ app/controllers/taxis_controller.rb
@@ -1,5 +1,46 @@
 class TaxisController < ApplicationController
+  before_action :set_taxi, only: [:edit, :show]
+
   def index
     @taxis = Taxi.all
   end
+
+  def new
+    @taxi = Taxi.new
+  end
+
+  def create
+    Taxi.create(taxi_params)
+
+  end
+
+  def destroy
+    taxi = Taxi.find(params[:id])
+    taxi.destroy
+  end
+
+  def edit
+    
+  end
+
+  def update
+    taxi = Taxi.find(params[:id])
+    taxi.update(taxi_params)
+  end
+
+  def show
+    
+  end
+
+  private
+
+  def taxi_params
+    params.require(:taxi).permit(:name, :image, :text)
+  end
+
+  def set_taxi
+    @taxi = Taxi.find(params[:id])
+  end
+
 end
+
