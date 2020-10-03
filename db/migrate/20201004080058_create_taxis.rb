b71319dd16b5f35357b0956db7660cc230c920e3
naocho <kirateiru831@gmail.com>
2020-10-03 16:39:13 +0900

    ユーザー投稿変種機能

--- db/migrate/20201004080058_create_taxis.rb
+++ db/migrate/20201004080058_create_taxis.rb
@@ -1,6 +1,7 @@
 class CreateTaxis < ActiveRecord::Migration[6.0]
   def change
     create_table :taxis do |t|
+      t.references :user,                null: false, foreign_key: true
       t.string :name
       t.string :text
       t.text :image
