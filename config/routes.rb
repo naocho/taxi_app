92a3cca0a6e4e10e0ae6bf1739809df7730b6999
naocho <kirateiru831@gmail.com>
2020-10-10 14:01:54 +0900

    heroku修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "taxis#index"
+  root "taxis#index"
   resources :taxis do
     resources :comments, only: :create
     collection do
